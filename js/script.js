fcbc6f7dc207a9acc0fd400f695c6fa25a8c523c
AntonellaBorasio <antonella.borasio@gmail.com>
2022-09-23 10:45:46 -0300

    Agregados ID y values para los botones

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Capturar los ingresos de números
+// Capturar los/el ingreso de operación
+// Almacenar los datos de ingreso (variables)
+// Capturo la ruta del resultado/pantalla
+// Crear los eventos de los botones (que recolecten los datos numéricos y de operación, y busque una función)
+
+let num1 = parseInt(document.querySelector("#n1").value);
+let num2 = parseInt(document.querySelector("#n3").value);
+let op = document.querySelector("#suma").value;
+
+console.log(num1);
+console.log(typeof num1);
+
+function suma(num1, num2, op) {}
